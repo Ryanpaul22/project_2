d017b0881999cf244076489b48641f2a24f26fc1
Ryan Paul <ryan.paul22@yahoo.com>
2019-02-13 12:51:30 -0500

    calendar.html, .js, and .css changes

--- db/loginSeeds.sql
+++
@@ -1,4 +0,0 @@
-USE login_DB;
-
-INSERT INTO members(email, password) 
-VALUES ('ryan.r.paul22@gmail.com','codingbootcamp')
--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,4 @@
-DROP DATABASE IF EXISTS lifingbuddies_db;
+DROP DATABASE IF EXISTS sequelize_json;
 
-CREATE DATABASE liftingbuddies_db;
+CREATE DATABASE sequelize_json;
 
-USE liftingbuddies_db;
-
-CREATE TABLE workouts (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(45) NULL,
-  muscleGroup VARCHAR(45) NULL,
-  exerNum INT NOT NULL,
-  exercise VARCHAR(45) NULL,
-  setNum INT NOT NULL,
-  repNum int not null,
-  PRIMARY KEY (id)
-);
-
--- just for push
-  
